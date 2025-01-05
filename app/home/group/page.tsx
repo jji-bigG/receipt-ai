053e8e493225c5fd5b3849196db73d8c5467dc45
jerry ji <jji.bigg@gmail.com>
2025-01-04 17:12:18 -0800

    outline the pages needed to be created

---
+++ app/home/group/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { GroupCard } from "./group-card";
+
+const Group = () => {
+  return (
+    <div>
+      <h1>Group</h1>
+      <GroupCard />
+    </div>
+  );
+};
+
+export default Group;
